1c9e451bc0277d60b328e3dd2185bb95a62fefee
Agustin Rojas <agustin.rojas@despegar.com>
2020-06-17 15:23:23 -0300

    Authorization Code Retrieval

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import urllib
 import os
-import urllib
+from flask import abort, request, Flask
+
+app = Flask(__name__)
 
 CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
 CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
 REDIRECT_URI = "http://localhost:65010/reddit_callback"
 
 
-app = Flask(__name__)
 
 @app.route('/')
 def homepage():
 	text = '<a href="%s">Authenticate with reddit</a>'
 	return text % make_authorization_url()
+
+@app.route('/reddit_callback')
+def reddit_callback():
+	error = request.args.get('error', '')
+	if error:
+		return "Error: " + error
+	state = request.args.get('state', '')
+	if not is_valid_state(state):
+		# Uh-oh, this request wasn't started by us!
+		abort(403)
+	code = request.args.get('code')
+	# We'll change this next line in just a moment
+	return "got a code! %s" % code
+
 
 def make_authorization_url():
 	# Generate a random string for the state parameter
