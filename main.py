3c99cc44bd50ee79078aed741818e215da79f005
Agustin Rojas <rojasagustin90@gmail.com>
2021-10-27 22:41:28 -0300

    add step_5

--- main.py
+++ main.py
@@ -11,11 +11,32 @@ CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
 CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
 REDIRECT_URI = "http://localhost:65010/reddit_callback"
 
+# Global variables
+access_token = None
+global_state = None
+
 
 @app.route('/')
 def homepage():
     text = '<a href="%s">Authenticate with reddit</a>'
     return text % make_authorization_url()
+
+
+def make_authorization_url():
+    # Generate a random string for the state parameter
+    # Save it for use later to prevent xsrf attacks
+    from uuid import uuid4
+    state = str(uuid4())
+    save_created_state(state)
+    params = {"client_id": CLIENT_ID,
+              "response_type": "code",
+              "state": state,
+              "redirect_uri": REDIRECT_URI,
+              "duration": "temporary",
+              "scope": "identity, read"}
+    url = "https://ssl.reddit.com/api/v1/authorize?" + \
+          urllib.parse.urlencode(params)
+    return url
 
 
 @app.route('/reddit_callback')
@@ -31,28 +52,11 @@ def reddit_callback():
     # We'll change this next line in just a moment
 
     try:
+        global access_token
         access_token = get_token(code)
-        user_name = get_username(access_token)
-        return "Your reddit username is: %s" % user_name
+        return access_token
     except Exception:
         return "ERROR"
-
-
-def make_authorization_url():
-    # Generate a random string for the state parameter
-    # Save it for use later to prevent xsrf attacks
-    from uuid import uuid4
-    state = str(uuid4())
-    save_created_state(state)
-    params = {"client_id": CLIENT_ID,
-              "response_type": "code",
-              "state": state,
-              "redirect_uri": REDIRECT_URI,
-              "duration": "temporary",
-              "scope": "identity"}
-    url = "https://ssl.reddit.com/api/v1/authorize?" + \
-          urllib.parse.urlencode(params)
-    return url
 
 
 def get_token(code):
@@ -60,7 +64,8 @@ def get_token(code):
     post_data = {"grant_type": "authorization_code",
                  "code": code,
                  "redirect_uri": REDIRECT_URI}
-    response = requests.post("https://ssl.reddit.com/api/v1/access_token",headers = {'User-agent': 'your bot 0.1'},
+    response = requests.post("https://ssl.reddit.com/api/v1/access_token",
+                             headers={'User-agent': 'your bot 0.1'},
                              auth=client_auth,
                              data=post_data)
 
@@ -68,26 +73,33 @@ def get_token(code):
     return token_json["access_token"]
 
 
-def get_username(access_token):
+@app.route('/userdata')
+def get_username():
+    global access_token
     headers = {"Authorization": "bearer " + access_token, 'User-agent': 'your bot 0.1'}
     response = requests.get(
         "https://oauth.reddit.com/api/v1/me", headers=headers)
+    return response.json()
 
-    me_json = response.json()
-    return me_json['name']
-    
 
-# Left as an exercise to the reader.
-# You may want to store valid states in a database or memcache,
-# or perhaps cryptographically sign them and verify upon retrieval.
+@app.route('/friends')
+def get_friends():
+    global access_token
+    headers = {"Authorization": "bearer " + access_token, 'User-agent': 'your bot 0.1'}
+    response = requests.get(
+        "https://oauth.reddit.com/api/v1/me/friends", headers=headers)
+
+    return response.json()
 
 
 def save_created_state(state):
-    pass
+    global global_state
+    global_state = state
 
 
 def is_valid_state(state):
-    return True
+    global global_state
+    return global_state == state
 
 
 if __name__ == '__main__':
