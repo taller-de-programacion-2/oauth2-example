5096128c39f7b04b6958463ee429942984d8d88d
Agustin Rojas <rojasagustin90@gmail.com>
2022-11-03 14:56:22 -0300

    update example

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
 import urllib
 import os
 from flask import Flask
+from dotenv import load_dotenv
 
-CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
-CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
+load_dotenv()
+
+CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
+CLIENT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
 REDIRECT_URI = "http://localhost:65010/reddit_callback"
-
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-	text = '<a href="%s">Authenticate with reddit</a>'
-	return text % make_authorization_url()
+    text = '<a href="%s">Authenticate with reddit</a>'
+    return text % make_authorization_url()
+
 
 def make_authorization_url():
-	# Generate a random string for the state parameter
-	# Save it for use later to prevent xsrf attacks
-	from uuid import uuid4
-	state = str(uuid4())
-	save_created_state(state)
-	params = {"client_id": CLIENT_ID,
-			  "response_type": "code",
-			  "state": state,
-			  "redirect_uri": REDIRECT_URI,
-			  "duration": "temporary",
-			  "scope": "identity"}
-	url = "https://ssl.reddit.com/api/v1/authorize?" + urllib.parse.urlencode(params)
-	return url
+    # Generate a random string for the state parameter
+    # Save it for use later to prevent xsrf attacks
+    from uuid import uuid4
+    state = str(uuid4())
+    save_created_state(state)
+    params = {"client_id": CLIENT_ID,
+              "response_type": "code",
+              "state": state,
+              "redirect_uri": REDIRECT_URI,
+              "duration": "temporary",
+              "scope": "identity"}
+    url = "https://ssl.reddit.com/api/v1/authorize?" + urllib.parse.urlencode(params)
+    return url
+
 
 # Left as an exercise to the reader.
 # You may want to store valid states in a database or memcache,
 # or perhaps cryptographically sign them and verify upon retrieval.
 def save_created_state(state):
-	pass
+    pass
+
+
 def is_valid_state(state):
-	return True
+    return True
 
 
 if __name__ == '__main__':
-	app.run(debug=True, port=65010)
+    app.run(debug=True, port=65010)
